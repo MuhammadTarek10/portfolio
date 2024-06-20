1efc850424cf46a10ad0c6c0870c8fbe99ce9d30
Muhammad Tarek <mohammadtarek000@gmail.com>
2024-06-20 19:19:07 +0300

    finished but with past data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,29 @@
 "use client";
 
+import { navItems } from "@/data";
+
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="relative bg-black-100 flex flex-col justify-center items-center sm:px-10 overflow-hidden px-5 mx-auto">
-      <div className="w-full max-w-7xl">
-        <FloatingNav
-          navItems={[
-            {
-              name: "Home",
-              link: "#",
-            },
-            {
-              name: "About",
-              link: "#about",
-            },
-            {
-              name: "Projects",
-              link: "#projects",
-            },
-            {
-              name: "Contact",
-              link: "#contact",
-            },
-          ]}
-        />
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Footer />
       </div>
     </main>
   );
-}
+};
+
+export default Home;
